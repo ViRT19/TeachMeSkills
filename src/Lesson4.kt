997ad36f942fb876e04226be560f642b0daa4a8c
ViRT <94885701+ViRT19@users.noreply.github.com>
2025-09-05 19:47:49 +0300

    Update Lesson4.kt

--- src/Lesson4.kt
+++ src/Lesson4.kt
@@ -11,6 +11,43 @@
            Напишите программу, которая проверяет, является ли массив чисел палиндромом.
  */
 
-fun main(){
+fun main() {
+    val maxIndex = 5 //Максимальный индекс
+    val maxNum = 10  // Максимальное значение элемента массива
+    val countIndex: Int = Randomize(maxIndex)           // Случайное значение количества элементов массива
+    val arr: Array<Int> = Array(countIndex, { 0 })    // Инициализируем массив типа Int, все нули
+    print("Сгенерирован массив из $countIndex элементов:")
+    for (i in 0..countIndex - 1) {                          // Заполнение массива случайными числами
+        arr[i] = Randomize(maxNum)
+        print(" ")
+        print(arr[i])
+    }
+    println(".")
+    var maxIndexOfArray: Int = 0
+    var minIndexOfArray: Int = 0
+    var countEven = 0   // Четные
+    var countOdd = 0    // Нечетные
 
+    for (i in 0..countIndex - 1) {
+        if (arr[i] > arr[maxIndexOfArray]) maxIndexOfArray = i
+        if (arr[i] < arr[minIndexOfArray]) minIndexOfArray = i
+        if (arr[i] % 2 == 0) countEven++ else countOdd++
+    }
+    println("Максимальное число = ${arr[maxIndexOfArray]} с индексом $maxIndexOfArray.")
+    println("Минимальное число = ${arr[minIndexOfArray]} с индексом $minIndexOfArray.")
+    println("Четных элементов: $countEven, нечётных: $countOdd.")
+    print("Массив в обратном порядке:")
+    for (i in countIndex - 1 downTo 0)
+        print(" ${arr[i]}")
+    println(".")
+
+    var palindrom: Boolean = true
+    for (i in 1..countIndex / 2)
+        if (arr[i - 1] != arr[countIndex - i])
+            palindrom = false
+    if (palindrom) println("Массив является палиндромом.") else println("Массив не палиндром.")
+}
+
+fun Randomize(maxNum: Int): Int {
+    return (Math.random() * maxNum).toInt() + 1
 }
