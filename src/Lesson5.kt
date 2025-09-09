3fa89efebdb7d2a357a7fa8bec3833321ce811eb
ViRT19 <rte_@mail.ru>
2025-09-09 15:35:32 +0300

    Lesson5. Task2 begin

--- src/Lesson5.kt
+++ src/Lesson5.kt
@@ -47,10 +47,17 @@ fun main() {
             cnt++
         }
     }
-    print("Отсортированный по возрастанию массив из $countIndex элементов:")
+    print("Отсортированный массив с сохранением порядка внутри групп из $countIndex элементов:")
     for (i in 0..countIndex - 1) print(" ${newArr[i]}")
     println(".")
 
 // Задание 2
+    cnt = 0
+    var res0: Array<Int> = Array(countIndex, { 0 })
+    var res1: Array<Int> = Array(countIndex, { 0 })
+    var res2: Array<Int> = Array(countIndex, { 0 })
+    var r1: Int = 0
+    for (i in 0..arr.size - 1)  // Дублируем массив arr в res0
+        res0[i] = arr[i]
 
 }
