a68a261fcf647aa73b5df2e1929a0ae57cc6bc91
ViRT19 <rte_@mail.ru>
2025-09-11 09:52:25 +0300

    Lesson5. Done.

--- src/Lesson5.kt
+++ src/Lesson5.kt
@@ -29,7 +29,7 @@ fun main() {
     var out = StringBuilder()
     for (i in newString.size - 1 downTo 0)
         out.append(newString[i]).append(" ")
-    println("2 способ. Перевёрнутая строка: «$out».")
+    println("2 способ. Перевёрнутая строка: «${out.trim()}».")
     /*************************************************************************/
     newString = newStr.reversed().split(" ")
     print("3 способ. Переворачиваем обратно: «")
