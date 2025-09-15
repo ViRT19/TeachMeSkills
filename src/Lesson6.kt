8682b1b3fb035b039bf17a74a9b9eb65679e0674
ViRT19 <rte_@mail.ru>
2025-09-15 16:33:56 +0300

    Lesson6 only 1

--- src/Lesson6.kt
+++ src/Lesson6.kt
@@ -19,6 +19,8 @@ fun RandomNumber(maxNum: Int): Int {
     return (Math.random() * maxNum).toInt() + 1
 }
 
+private fun Map<Int, Int>.sortedWith(thenBy: Comparator<Int>) {}
+
 fun main() {
     val maxIndex = 20 //Максимальный индекс
     val maxNum = 5  // Максимальное значение элемента массива
@@ -47,53 +49,10 @@ fun main() {
             cnt++
         }
     }
-    print("Ответ: отсортированный массив с сохранением порядка внутри групп из $countIndex элементов:")
-    for (i in 0..countIndex - 1) print(" ${newArr[i]}")
-    println(".")
+    print("Ответ: отсортированный массив с сохранением порядка внутри групп:")
+    newArr.forEach { print(" $it") }
+    println(".\n--------------------------------------------------------")
 
 
-// Задание 2
-    var res0: Array<Int> = Array(countIndex, { 0 })
-    arr.sort<Int>().also { res0 = arr }
-    print("Задание 2, неоконченное. Первая сортировка: ")
-    for (i in 0..res0.size - 1)
-        print("${res0[i]} ")
-    println()
 
-
-// Задание 3
-    print("Задание 3. Сгенерирован массив из $countIndex элементов:")
-    for (i in 0..countIndex - 1) {                          // Заполнение массива случайными числами
-        arr[i] = RandomNumber(maxNum) - maxNum / 2               //Добавляем отрицательные числа
-        print(" ")
-        print(arr[i])
-    }
-    println(".")
-    var max: Int = 0
-    var maxI: Int = 0
-    var min: Int = 0
-    var minI: Int = 0
-    var res3: Array<Int> = Array(countIndex, { 0 })
-    var countIterations: Int = 0
-    for (i in 0..countIndex - 1) {
-        if (min > arr[i]) {
-            min = arr[i]
-            minI = i
-        }
-        if (max < arr[i]) {
-            max = arr[i]
-            maxI = i
-        }
-    }
-    arr[minI] = arr[0]
-    arr[0] = min
-    arr[maxI] = arr[countIndex - 1]
-    arr[countIndex - 1] = max
-    countIterations++
-    res3[0] = min
-    res3[countIndex - 1] = max
-    println("Max = $max, Min = $min")
-
-    res3[0] = arr.min()
-    res3[countIndex - 1] = arr.max()
 }
