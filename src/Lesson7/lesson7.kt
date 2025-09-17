dceac991474827c4e84373db0ab173ef9a1a6e89
ViRT19 <rte_@mail.ru>
2025-09-17 07:23:09 +0300

    Lesson7. Task description.

---
+++ src/Lesson7/lesson7.kt
@@ -0,0 +1,15 @@
+package Lesson7
+/*
+Создать класс Book с:полями title, author, year
+первичным конструктором.методом toString().
+Добавить метод equals() и hashCode() для сравнения книг по названию и автору.
+Создать enum class Genre (FANTASY, DETECTIVE, SCIENCE, HISTORY).
+Сделать абстрактный класс LibraryItem (с полем id) и наследовать от него Book.
+
+Создать interface Borrowable с методом borrow(user: String) и реализовать его в Book.
+И создать аналог Book через Data class и сравнить логику методов tostring, equals, hashcode
+
+ */
+fun main() {
+
+}
