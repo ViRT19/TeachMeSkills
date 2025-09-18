f0248e3a8efcd4840a9219d78ddfff24263e3685
ViRT19 <rte_@mail.ru>
2025-09-18 10:15:48 +0300

    Lesson7. Done.

--- src/Lesson7/lesson7.kt
+++ src/Lesson7/lesson7.kt
@@ -8,8 +8,16 @@ package Lesson7
 
 Создать interface Borrowable с методом borrow(user: String) и реализовать его в Book.
 И создать аналог Book через Data class и сравнить логику методов tostring, equals, hashcode
-
  */
+import Lesson7.Genre
 fun main() {
+    val b1 = Book("Космобиолухи", "Ольга Громыко", 2011, Genre.FANTASY,1,)
+    val b2 = Book("Аквариум", "Виктор Суворов", 1991, Genre.DETECTIVE,2,)
+    val b3 = Book("Ледокол", "Виктор Суворов", 1998, Genre.HISTORY,3,)
+    println(b1)
+    println(b2)
+    println(b3)
+    println("b3 == b2 ${b3 == b2}")
+    println("b1 == b2 ${b1 == b2}")
 
 }
