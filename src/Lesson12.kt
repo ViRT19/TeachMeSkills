fe4980e1086df5e67a122f06c92a572c297bc975
ViRT19 <rte_@mail.ru>
2025-10-03 07:35:36 +0300

    Lesson 12 done.

--- src/Lesson12.kt
+++ src/Lesson12.kt
@@ -1,11 +1,12 @@
-fun anagramma() {
-    val a: String = "hvdjksavklsad vlsavg"
-    val b: String = "vhdjksavklsad vlsavg"
+fun anagramm(a: String, b: String): Boolean {
     a.uppercase()
     b.uppercase()
     var chA = a.toList().sorted()
     var chB = b.toList().sorted()
-    if (chA == chB) println("Aнаграмма") else println("Это не анаграмма")
+    if (chA == chB)
+        return true
+    else
+        return false
 }
 /*
 Задача 1: Группировка анаграмм
@@ -16,5 +17,19 @@ fun anagramma() {
  */
 
 fun main() {
-    anagramma()
+    println("Задание 1. Найти анаграммы из списка: «listen», «silent», «enlist», «java», «avaj», «world».")
+    val list = listOf<String>("listen", "silent", "enlist", "java", "avaj", "world")
+    for (i in 0..list.size - 1)
+        for (j in 1..list.size - 1)
+            if (anagramm(list[i], list[j]) and (list[i].toList() != list[j].toList()))
+                println("Анаграммы: «${list[i]}» и «${list[j]}».")
+
+    println("Задание 2. Найти первое повторяющееся число из списка чисел.")
+    val listNum = listOf<Int>(4, 6, 8, 6, 5, 3, 4, 6, 7, 56, 5, 4, 5, 6, 6, 5, 4, 3, 2, 2, 4, 5, 6, 67, 7)
+    for (i in 0..listNum.size - 1)
+        for (j in 1..listNum.size - 1)
+            if (listNum[i] == listNum[j]) {
+                println("Первое повторяющееся число = ${listNum[i]}")
+                return
+            }
 }
