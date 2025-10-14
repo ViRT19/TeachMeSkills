75493479338ac0bbac1098263f1f0a0e351cf209
ViRT19 <rte_@mail.ru>
2025-10-14 19:09:54 +0300

    Lesson14

--- src/Lesson14.kt
+++ src/Lesson14.kt
@@ -1,3 +1,8 @@
+import java.util.concurrent.atomic.AtomicInteger
+import kotlin.concurrent.atomics.AtomicInt
+import kotlin.concurrent.atomics.ExperimentalAtomicApi
+import kotlin.concurrent.thread
+
 /*
 Задача 1: Синхронизация доступа к счетчику с помощью внешнего объекта.
           Вам нужно создать класс Counter, который будет иметь метод increment и метод decrement.
@@ -11,6 +16,66 @@
           Ваша задача — обеспечить, чтобы операция увеличения и уменьшения была выполнена безопасно с помощью синхронизации.
 */
 
-fun main() {
 
+object ExternalObject               // Внешний объект. Нужен в качестве монитора
+
+class Counter {
+    private var count = 0
+    fun increment() {
+        synchronized(ExternalObject) {  // Первый метод синхронизации
+            count++
+        }
+    }
+
+    @Synchronized               // Второй метод синхронизации
+    fun decrement() {
+        count--
+    }
+
+    fun getCount() = count
+}
+
+class CounterAtomic() {
+    private var cntAtomic = AtomicInteger(0)
+    fun incAtomic() {
+        cntAtomic.incrementAndGet()
+    }
+
+    fun decAtomic() {
+        cntAtomic.decrementAndGet()
+    }
+
+    fun getAtomic() = cntAtomic
+}
+
+fun main() {
+    val counterVal = Counter()
+    val counterInc = Thread {
+        for (i in 1..100)
+            counterVal.increment()
+    }
+    val counterDec = Thread {
+        for (i in 1..100)
+            counterVal.decrement()
+    }
+    counterInc.start()
+    counterDec.start()
+    counterInc.join()
+    counterDec.join()
+    println(counterVal.getCount())
+
+    val counterAtomic = CounterAtomic()
+    val counterAtomicInc = Thread {
+        for (i in 1..100)
+            counterAtomic.incAtomic()
+    }
+    val counterAtomicDec = Thread {
+        for (i in 1..100)
+            counterAtomic.decAtomic()
+    }
+    counterAtomicInc.start()
+    counterAtomicDec.start()
+    counterAtomicInc.join()
+    counterAtomicDec.join()
+    println(counterAtomic.getAtomic())
 }
