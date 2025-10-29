3d5594d1bcf6fa94c796f56411e25af74b67efc8
ViRT19 <rte_@mail.ru>
2025-10-29 08:05:13 +0300

    Switch Activities

---
+++ Android/Lesson16-17/app/src/main/java/com/example/lesson16_17/MainActivity.kt
@@ -0,0 +1,35 @@
+package com.example.lesson16_17
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        content()
+    }
+
+    fun content() {
+        val btn = findViewById<Button>(R.id.btn)
+        btn.setOnClickListener {
+            val intent2 = Intent(this, SecondActivity::class.java)
+            intent2.putExtra("txt", R.string.main_activity)
+            startActivity(intent2)
+        }
+        val txt: TextView = findViewById<TextView>(R.id.tv)
+        txt.text = intent.getStringExtra("txt")
+    }
+}
--- Android/Lesson16Activities/app/src/main/java/com/example/activities/Object.kt
+++
@@ -1,7 +0,0 @@
-package com.example.activities
-
-object Object {
-    val logTag = "LogTag"
-    val keyOfCount = "KeyOfSwitch"
-    var countSwitchMyActivity = 0
-}
