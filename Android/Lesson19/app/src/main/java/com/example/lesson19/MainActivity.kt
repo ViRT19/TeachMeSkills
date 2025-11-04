42cdc8a335e7d6266fdf6fa4979c47b5f96a8322
ViRT19 <rte_@mail.ru>
2025-11-04 16:32:07 +0300

    Lesson19 new edition

---
+++ Android/Lesson19/app/src/main/java/com/example/lesson19/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.example.lesson19
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        supportFragmentManager
+            .beginTransaction()
+            .replace(R.id.place_holder, BlankFragment.newInstance())
+            .commit()
+    }
+}
