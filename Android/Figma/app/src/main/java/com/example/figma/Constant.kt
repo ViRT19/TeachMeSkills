6822c138f4b4e6a04161c984d2223335dac3a3d2
ViRT19 <rte_@mail.ru>
2025-11-19 07:47:40 +0300

    Runed app.

---
+++ Android/Figma/app/src/main/java/com/example/figma/Constant.kt
@@ -0,0 +1,4 @@
+package com.example.figma
+
+lateinit var MAIN: MainActivity
+var NAME = "Android"
--- Android/Figma/app/src/main/java/com/example/figma/MainActivity.kt
+++ Android/Figma/app/src/main/java/com/example/figma/MainActivity.kt
@@ -4,12 +4,22 @@ import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
+import androidx.core.view.WindowCompat.enableEdgeToEdge
 import androidx.core.view.WindowInsetsCompat
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import com.example.figma.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
+    lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        navController = Navigation.findNavController(this, R.id.nav_host_fragment)
+        MAIN = this
     }
 }
