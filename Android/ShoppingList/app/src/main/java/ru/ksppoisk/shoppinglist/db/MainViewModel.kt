ca9718b8636f159af7e9f325a9580dc18b07f231
ViRT19 <rte_@mail.ru>
2025-12-04 07:47:57 +0300

    View Model

---
+++ Android/ShoppingList/app/src/main/java/ru/ksppoisk/shoppinglist/db/MainViewModel.kt
@@ -0,0 +1,27 @@
+package ru.ksppoisk.shoppinglist.db
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.asLiveData
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.launch
+import ru.ksppoisk.shoppinglist.entities.NoteItem
+
+class MainViewModel(database: MainDataBase) : ViewModel() {
+    val dao = database.getDao()
+    val allNotes: LiveData<List<NoteItem>> = dao.getAllNotes().asLiveData()
+    fun insertNote(note: NoteItem) = viewModelScope.launch {
+        dao.insertNote(note)
+    }
+
+    class MainViewModelFactory(val database: MainDataBase) : ViewModelProvider.Factory {
+        override fun <T : ViewModel?> create(modelClass: Class<T>): T {
+            if (modelClass.isAssignableFrom(MainViewModel::class.java)) {
+                @Suppress("UNCHECKED_CAST")
+                return MainViewModel(database) as T
+            }
+            throw IllegalArgumentException("Unknown ViewModelClass")
+        }
+    }
+}
--- Android/ShoppingList/app/src/main/java/ru/ksppoisk/shoppinglist/fragments/NoteFragment.kt
+++ Android/ShoppingList/app/src/main/java/ru/ksppoisk/shoppinglist/fragments/NoteFragment.kt
@@ -5,11 +5,18 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.activityViewModels
 import ru.ksppoisk.shoppinglist.R
+import ru.ksppoisk.shoppinglist.activities.MainApp
 import ru.ksppoisk.shoppinglist.databinding.FragmentNoteBinding
+import ru.ksppoisk.shoppinglist.db.MainViewModel
 
 class NoteFragment : BaseFragment() {
     private lateinit var binding: FragmentNoteBinding
+    private val mainViewModel: MainViewModel by activityViewModels() {
+        MainViewModel.MainViewModelFactory((context?.applicationContext as MainApp).database)
+    }
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -20,6 +27,10 @@ class NoteFragment : BaseFragment() {
 
     override fun onClickNew() {
 
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
     }
 
     companion object {
